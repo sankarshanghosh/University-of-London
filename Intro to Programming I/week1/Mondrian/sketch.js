35c5fbebac472dcec7601cea6ef6764c4dde1c84
Sankarshan Ghosh <sankarshanghosh19@gmail.com>
2022-11-10 22:21:41 +0530

    .

--- Intro to Programming I/week1/Mondrian/sketch.js
+++ Intro to Programming I/week1/Mondrian/sketch.js
@@ -1,19 +1,12 @@
 function setup() {
-  //create a large square canvas
-  createCanvas(800, 800);
+  createCanvas(500, 500);
 }
 
 function draw() {
-  //set the fill colour to red
   fill(255, 0, 0);
+  rect(100, 100, 300, 300);
+}
 
-  //set a thick stroke weight for the black lines
-  strokeWeight(12);
-
-  //draw the red rectangle
-  rect(100, 50, 600, 600);
+function mouseReleased() {
   fill(0, 0, 255);
-  rect(0, 50, 100, 300);
-  fill(220, 220, 220);
-  rect(0, 350, 100, 300);
 }
