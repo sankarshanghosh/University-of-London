f3dc864f24f5197290c913b9e2e48b4e2c8da7d6
Sankarshan Ghosh <50132419+sankarshanghosh@users.noreply.github.com>
2024-06-14 15:41:25 +0530

    14jun341

--- Databases, Networks and the Web/Preparation/Starter_Files/topic4/htmlExpress/routes/main.js
+++ Databases, Networks and the Web/Preparation/Starter_Files/topic4/htmlExpress/routes/main.js
@@ -1,1 +1,12 @@
 // The main.js file of your application
+module.exports = function (app) {
+  app.get("/", function (req, res) {
+    res.render("index.html");
+  });
+  app.get("/about", function (req, res) {
+    res.render("search.html");
+  });
+  app.get("/search", function (req, res) {
+    res.render("search.html");
+  });
+};
