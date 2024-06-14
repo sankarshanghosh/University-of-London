af4d0f2b6341e1d372ed7c6ad8156ad0bb324fc1
Sankarshan Ghosh <50132419+sankarshanghosh@users.noreply.github.com>
2024-06-14 20:51:59 +0530

    ..

--- Databases, Networks and the Web/Preparation/Starter_Files/topic4/htmlExpress/index.js
+++ Databases, Networks and the Web/Preparation/Starter_Files/topic4/htmlExpress/index.js
@@ -1,8 +1,14 @@
 // The index.js file of your application
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
 const port = 8086;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
 require("./routes/main")(app);
+
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
 app.engine("html", require("ejs").renderFile);
--- Databases, Networks and the Web/Preparation/Starter_Files/topic4/htmlExpress/routes/main.js
+++ Databases, Networks and the Web/Preparation/Starter_Files/topic4/htmlExpress/routes/main.js
@@ -9,4 +9,15 @@ module.exports = function (app) {
   app.get("/search", function (req, res) {
     res.render("search.html");
   });
+  app.get("/search-result", function (req, res) {
+    //searching in the database
+    res.send(req.query);
+  });
+  app.get("/register", function (req, res) {
+    res.render("register.html");
+  });
+  app.post("/registered", function (req, res) {
+    // saving data in database
+    res.send("Hello "+ req.body.first + " "+ req.body.last +", you are now registered!");
+  });
 };
