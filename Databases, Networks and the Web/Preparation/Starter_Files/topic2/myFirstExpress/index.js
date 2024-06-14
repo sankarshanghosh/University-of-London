6e2809938ddcf1d9fbdf5a119864c416ef3a421f
Sankarshan Ghosh <50132419+sankarshanghosh@users.noreply.github.com>
2024-06-14 11:14:36 +0530

    14jun

--- Databases, Networks and the Web/Preparation/Starter_Files/topic2/myFirstExpress/index.js
+++ Databases, Networks and the Web/Preparation/Starter_Files/topic2/myFirstExpress/index.js
@@ -3,6 +3,5 @@ const express = require("express");
 const app = express();
 const port = 8083;
 app.get("/", (req, res) => res.send("<h1>Hello World!</h1>"));
+app.get("/about", (req, res) => res.send("<h1>About Us</h1>"));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
-
