8503b3cc6da572ccd8f6f93f21eeb8e320cc9f36
Sankarshan Ghosh <50132419+sankarshanghosh@users.noreply.github.com>
2024-06-15 11:50:51 +0530

    done

--- Databases, Networks and the Web/Preparation/Starter_Files/topic6/htmlExpress/index.js
+++ Databases, Networks and the Web/Preparation/Starter_Files/topic6/htmlExpress/index.js
@@ -1,1 +1,16 @@
-// The index.js file of your application
+const mysql = require("mysql");
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "@9P5M1R9#",
+  database: "myBookshop"
+});
+
+db.connect(err => {
+  if (err) {
+    console.error('Connection error: ', err);
+    return;
+  }
+  console.log('Connected to database');
+});
--- Databases, Networks and the Web/Preparation/Starter_Files/topic6/htmlExpress/routes/main.js
+++ Databases, Networks and the Web/Preparation/Starter_Files/topic6/htmlExpress/routes/main.js
@@ -1,1 +1,40 @@
 // The main.js file of your application
+// The main.js file of your application
+module.exports = function (app) {
+  app.get("/", function (req, res) {
+    res.render("index.html");
+  });
+  app.get("/about", function (req, res) {
+    res.render("about.html");
+  });
+  app.get("/search", function (req, res) {
+    res.render("search.html");
+  });
+  app.get("/search-result", function (req, res) {
+    //searching in the database
+    res.send(req.query);
+  });
+  app.get("/list", function (req, res) {
+    // query database to get all the books
+    let sqlquery = "SELECT * FROM book"; // execute sql query
+    db.query(sqlquery, (err, result) => {
+      if (err) {
+        res.redirect("/");
+      }
+      res.send(result);
+    });
+  });
+  app.get("/register", function (req, res) {
+    res.render("register.html");
+  });
+  app.post("/registered", function (req, res) {
+    // saving data in database
+    res.send(
+      "Hello " +
+        req.body.first +
+        " " +
+        req.body.last +
+        ", you are now registered!"
+    );
+  });
+};
