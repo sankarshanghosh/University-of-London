96a6293e9820222786eea9904a5f459ab081635d
Sankarshan Ghosh <50132419+sankarshanghosh@users.noreply.github.com>
2024-06-15 11:52:15 +0530

    ...

--- Databases, Networks and the Web/Preparation/Starter_Files/topic6/htmlExpress/index.js
+++ Databases, Networks and the Web/Preparation/Starter_Files/topic6/htmlExpress/index.js
@@ -3,7 +3,7 @@ const mysql = require("mysql");
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "@9P5M1R9#",
+  password: "",
   database: "myBookshop"
 });
 
